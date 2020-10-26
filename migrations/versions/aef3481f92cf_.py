751f46197b1ee3ce613eecc0babf4dfe2bbd3277
Christoph Becker <mail@ch-becker.de>
2020-10-26 06:22:03 +0100

    db init, endpoint set

---
+++ migrations/versions/aef3481f92cf_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: aef3481f92cf
+Revises: 
+Create Date: 2020-10-26 06:05:12.786771
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'aef3481f92cf'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- vatservice/__init__.py
+++ vatservice/__init__.py
@@ -1,22 +1,33 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
-db = SQLAlchemy()
+app = Flask(__name__)
+
+app.config.from_mapping(
+    SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev_key',
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+db = SQLAlchemy(app)
 migrate = Migrate()
+migrate.init_app(app, db)
+db.create_all()
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev_key',
-        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or
-                                'sqlite:///' + os.path.join(app.instance_path, 'local_vats.sqlite'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    )
-    db.init_app(app)
-    migrate.init_app(app, db)
+class Company(db.Model):
+    # https://de.wikipedia.org/wiki/Umsatzsteuer-Identifikationsnummer#Aufbau_der_Identifikationsnummer
+    vatNumber = db.Column(db.String(20), nullable=False, primary_key=True)
+    name = db.Column(db.Text(), nullable=True)
+    address = db.Column(db.Text(), nullable=True)
+    valid = db.Column(db.Boolean())
 
-    return app
+
+@app.route('/', methods=('GET',))
+def get_vat_info():
+    vatid = request.args.get('vatid')
+    if vatid is None:
+        return 'Need vatid as parameter to check', 400
